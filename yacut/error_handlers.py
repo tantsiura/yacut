d1f81093990ca78f474dc111e6fe3d6d3c369ccb
tantsiura <tantsiura.e@yandex.ru>
2023-10-28 19:32:02 +0300

    1st commit

---
+++ yacut/error_handlers.py
@@ -0,0 +1,24 @@
+from typing import Tuple
+from http import HTTPStatus
+
+from flask import Response, jsonify, render_template
+from werkzeug.exceptions import HTTPException
+
+from yacut import app, db
+from yacut.exceptions import APIError
+
+
+@app.errorhandler(APIError)
+def invalid_api_usage(error: APIError) -> Tuple[Response, int]:
+    return jsonify(error.to_dict()), error.status_code
+
+
+@app.errorhandler(404)
+def page_not_found(error: HTTPException) -> Tuple[str, int]:
+    return render_template('errors/404.html'), HTTPStatus.NOT_FOUND
+
+
+@app.errorhandler(500)
+def internal_error(error: HTTPException) -> Tuple[str, int]:
+    db.session.rollback()
+    return render_template('errors/500.html'), HTTPStatus.INTERNAL_SERVER_ERROR
