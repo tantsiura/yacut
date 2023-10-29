7113401a07bcb599dcb942bac82341423e9b28da
tantsiura <tantsiura.e@yandex.ru>
2023-10-29 10:33:29 +0300

    fix

--- yacut/exceptions.py
+++
@@ -1,7 +0,0 @@
-
-    Проверяет допустимость символов.
-    """
-    def __call__(self, form, field):
-        if self.message is None:
-            self.message = f'Some element of {field.data} not in {self.values}'
-        symbols_validation(field.data, validators.ValidationError(self.message))
