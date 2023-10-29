e19ded0126add70446a8b0d4ff6434f68283d806
tantsiura <tantsiura.e@yandex.ru>
2023-10-29 10:23:40 +0300

    fix

--- yacut/constants.py
+++ yacut/constants.py
@@ -7,5 +7,5 @@ SHORT_ID_LENGTH = 6
 EMPTY_REQUEST_BODY = 'Отсутствует тело запроса'
 FIELD_IS_REQUIRED = '"{field}" является обязательным полем!'
 INVALID_CUSTOM_ID = 'Указано недопустимое имя для короткой ссылки'
-NOT_UNIQUE_CUSTOM_ID = 'Имя "{custom_id}" уже занято.'
+NOT_UNIQUE_CUSTOM_ID = 'Имя "{custom_id}" уже занято!'
 SHORT_ID_NOT_FOUND = 'Указанный id не найден'
