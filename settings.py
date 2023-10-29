b77a4e6185e2d71bb11bb84989c428bd1356b169
tantsiura <tantsiura.e@yandex.ru>
2023-10-29 11:01:44 +0300

    fix

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 
-class Config:
+class Config(object):
+    SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', default='KEY_123')
