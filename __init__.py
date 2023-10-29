40ec82d1654e5073a349eb175fe2feb5c6e75238
tantsiura <tantsiura.e@yandex.ru>
2023-10-29 11:50:57 +0300

    fix

--- __init__.py
+++ __init__.py
@@ -5,8 +5,7 @@ from settings import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-#db = SQLAlchemy(app)
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from . import api_views, error_handlers, views
